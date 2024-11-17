d496007642fb7b0d580775d3f72c9e9ff89f9f82
Almir Sharifullin <a.sharifullinx@dodobrands.io>
2024-11-18 00:14:22 +0500

    https redirection

--- Bonds.App.Api/Program.cs
+++ Bonds.App.Api/Program.cs
@@ -37,6 +37,7 @@ namespace Bonds.App.Api
 
             var app = builder.Build();
             app.UseHangfireDashboard();
+            app.UseHttpsRedirection();
             RegisterJobs(app);
             using var scope = app.Services.CreateScope();
             scope.ServiceProvider.GetRequiredService<ITelegramClient>().Receive();
